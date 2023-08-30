591b499bd070bf904a110d4f639ae59405277b13
M4aXim <79996174+M4aXim@users.noreply.github.com>
2023-08-30 22:21:51 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,20 +1,32 @@
-function assignEditUrls() {
-  var form = FormApp.openById('14QLjmVDz9allJJszaoLqc_YHnEmxIeXUU46tYF-1-WU');
-
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('dep');
-
+function testkey() {
+  var form = FormApp.openById('1xXLR1PGbL7ecV9Cfc15oiKOuu1YQhUQl0lQRvuzH9nU');
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('res');
   var data = sheet.getDataRange().getValues();
-  var urlCol = 6; 
+  var urlCol = 6;
   var responses = form.getResponses();
   var timestamps = [], urls = [], resultUrls = [];
-  
+
+  // Populate timestamps and URLs arrays
   for (var i = 0; i < responses.length; i++) {
     timestamps.push(responses[i].getTimestamp().setMilliseconds(0));
     urls.push(responses[i].getEditResponseUrl());
   }
-  for (var j = 1; j < data.length; j++) {
 
-    resultUrls.push([data[j][0]?urls[timestamps.indexOf(data[j][0].setMilliseconds(0))]:'']);
+  // Process data and populate resultUrls
+  for (var j = 1; j < data.length; j++) {
+    var currentTimestamp = data[j][0];
+    var resultUrl = '';
+
+    if (currentTimestamp instanceof Date) {
+      var index = timestamps.indexOf(currentTimestamp.setMilliseconds(0));
+      if (index !== -1) {
+        resultUrl = urls[index];
+      }
+    }
+
+    resultUrls.push([resultUrl]);
   }
-  sheet.getRange(2, urlCol, resultUrls.length).setValues(resultUrls);  
+
+  // Update the spreadsheet with resultUrls
+  sheet.getRange(2, urlCol, resultUrls.length).setValues(resultUrls);
 }
